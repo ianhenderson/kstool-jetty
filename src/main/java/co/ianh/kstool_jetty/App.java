f215cb9f16c56e317dd76890331f85f8bd0af30a
Ian Henderson <ianwhen@gmail.com>
2016-03-23 16:33:44 -0700

    Move handler into own class

--- src/main/java/co/ianh/kstool_jetty/App.java
+++ src/main/java/co/ianh/kstool_jetty/App.java
@@ -1,33 +1,17 @@
 package co.ianh.kstool_jetty;
 
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 import static co.ianh.kstool_jetty.Utils.getPort;
 
 /**
  * Created by henderson_i on 3/22/16.
  */
-public class App extends AbstractHandler {
-
-    @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html; charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("Sup, beautiful world.");
-        baseRequest.setHandled(true);
-    }
+public class App {
 
     public static void main(String[] args) throws Exception {
         int port = getPort();
         Server server = new Server(port);
-        server.setHandler(new App());
+        server.setHandler(new Handler());
         server.start();
         server.join();
     }
