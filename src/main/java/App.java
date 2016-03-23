75f675458321a2f00a5f725cc90dafefbb57fc86
Ian Henderson <ianwhen@gmail.com>
2016-03-23 15:39:28 -0700

    Add basic helloworld handler

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by henderson_i on 3/22/16.
+ */
+public class App extends AbstractHandler {
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("Sup, beautiful world.");
+        baseRequest.setHandled(true);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new App());
+        server.start();
+        server.join();
+    }
+}
