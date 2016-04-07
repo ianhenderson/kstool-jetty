74fdee2b0a1fb3eba5241277b5d38f83f5d3bef6
Ian Henderson <ianwhen@gmail.com>
2016-04-06 17:04:58 -0700

    Refactor DAL

--- src/main/java/co/ianh/kstool_jetty/DataAccessLayer.java
+++ src/main/java/co/ianh/kstool_jetty/DataAccessLayer.java
@@ -2,6 +2,7 @@ package co.ianh.kstool_jetty;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -9,53 +10,47 @@ import java.sql.Statement;
  */
 public class DataAccessLayer {
 
-    private static Connection c = null;
+    private static Connection c = makeConnection();
+    private static final String DB_URL = "jdbc:sqlite:test.db";
 
     // Set up DB connection
-    public static void makeConnection() {
+    private static Connection makeConnection() {
+        Connection c = null;
         try {
-            if (c == null || c.isClosed()) { // connection already established
-                Class.forName("org.sqlite.JDBC");
-                c = DriverManager.getConnection("jdbc:sqlite:test.db");
-                c.setAutoCommit(false);
-            }
+            c = DriverManager.getConnection(DB_URL);
+            c.setAutoCommit(false);
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
         System.out.println("Opened DB Successfully!");
+        return c;
     }
 
     public static void initTables() {
-        makeConnection();
+        Statement stmt = null;
         try {
-            Statement stmt = null;
             stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name VARCHAR(255) UNIQUE, password VARCHAR(255), salt VARCHAR(255))";
-            stmt.executeUpdate(sql);
+
+            // Table of user data
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name VARCHAR(255) UNIQUE, password VARCHAR(255), salt VARCHAR(255))");
 
             // Tables of all unique kanji, words and a junction table
-            sql = "CREATE TABLE IF NOT EXISTS kanji (id INTEGER PRIMARY KEY, kanji TEXT UNIQUE)";
-            stmt.executeUpdate(sql);
-            sql = "CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, word TEXT UNIQUE)";
-            stmt.executeUpdate(sql);
-            sql = "CREATE TABLE IF NOT EXISTS kanji_words (kanji TEXT, word_id INTEGER, FOREIGN KEY(kanji) REFERENCES kanji(kanji), FOREIGN KEY(word_id) REFERENCES words(id), CONSTRAINT unq UNIQUE (kanji, word_id))";
-            stmt.executeUpdate(sql);
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS kanji (id INTEGER PRIMARY KEY, kanji TEXT UNIQUE)");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, word TEXT UNIQUE)");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS kanji_words (kanji TEXT, word_id INTEGER, FOREIGN KEY(kanji) REFERENCES kanji(kanji), FOREIGN KEY(word_id) REFERENCES words(id), CONSTRAINT unq UNIQUE (kanji, word_id))");
 
             // Tables of seen words/kanji on a per-user basis
-            sql = "CREATE TABLE IF NOT EXISTS seen_words (user_id INTEGER, word_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(word_id) REFERENCES words(id))";
-            stmt.executeUpdate(sql);
-            sql = "CREATE TABLE IF NOT EXISTS seen_kanji (user_id INTEGER, kanji TEXT, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(kanji) REFERENCES kanji(kanji))";
-            stmt.executeUpdate(sql);
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS seen_words (user_id INTEGER, word_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(word_id) REFERENCES words(id))");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS seen_kanji (user_id INTEGER, kanji TEXT, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(kanji) REFERENCES kanji(kanji))");
 
             // Queue of items to study for each user
-            sql = "CREATE TABLE IF NOT EXISTS study_queue (user_id INTEGER, queue TEXT, FOREIGN KEY(user_id) REFERENCES users(id))";
-            stmt.executeUpdate(sql);
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS study_queue (user_id INTEGER, queue TEXT, FOREIGN KEY(user_id) REFERENCES users(id))");
 
             stmt.close();
             c.commit();
-            c.close();
         } catch ( Exception e ) {
+//            c.rollback();
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
