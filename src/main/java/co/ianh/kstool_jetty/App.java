0e3be7b7f5f6b0625822ad0e0d90a4d6db277603
Ian Henderson <ianwhen@gmail.com>
2016-05-10 18:12:29 -0700

    Link static content to backend

--- src/main/java/co/ianh/kstool_jetty/App.java
+++ src/main/java/co/ianh/kstool_jetty/App.java
@@ -7,8 +7,8 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
 
 import static co.ianh.kstool_jetty.Utils.getHost;
 import static co.ianh.kstool_jetty.Utils.getPort;
@@ -28,7 +28,7 @@ public class App {
         int port = getPort();
         Server server = new Server(port);
 
-        // Build context
+        // Build servlet handler
         Handler servletHandler = buildServletHandler();
 
         // Set handler
@@ -37,17 +37,6 @@ public class App {
         // Start server
         server.start();
         return server;
-    }
-
-    private static Handler buildContext() throws Exception {
-        // Build up contexts to be handled
-        ContextHandlerCollection contexts = buildContexts();
-
-        // Add gzip to responses
-        GzipHandler gzip = new GzipHandler();
-        gzip.setHandler(contexts);
-
-        return gzip;
     }
 
     private static ServerConnector configureConnector(Server server) throws Exception {
@@ -66,37 +55,11 @@ public class App {
         return http; // TODO: is this even necessary?
     }
 
-    private static ContextHandlerCollection buildContexts() throws Exception {
-        // Hello handler
-        Handler hello = new co.ianh.kstool_jetty.Handler();
-
-        // Fileserver handler
-        ResourceHandler fs = FileServer.build();
-
-        // Create contextHandler for /
-        ContextHandler helloContext = new ContextHandler();
-        helloContext.setContextPath("/");
-        helloContext.setHandler(hello);
-
-        // Create contextHandler for /files
-        ContextHandler fileContext = new ContextHandler();
-        fileContext.setContextPath("/files");
-        fileContext.setHandler(fs);
-
-        Handler[] handlers = {
-                helloContext,
-                fileContext
-        };
-
-        // Create collection of contexts
-        ContextHandlerCollection contexts = new ContextHandlerCollection();
-        contexts.setHandlers(handlers);
-
-        return contexts;
-    }
-
     private static Handler buildServletHandler() throws Exception {
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        // Set base directory for static content
+        handler.setResourceBase("./kstool-client/public/angular-mat-design");
 
         // Routes
         handler.addServlet(KanjiServlet.class, "/api/kanji");
@@ -104,7 +67,7 @@ public class App {
         handler.addServlet(SignupServlet.class, "/api/signup");
         handler.addServlet(LoginServlet.class, "/api/login");
         handler.addServlet(LogoutServlet.class, "/api/logout");
-        handler.addServlet(HelloServlet.class, "/*");
+        handler.addServlet(DefaultServlet.class, "/");
 
         return handler;
     }
